2c19b58331f94c8e775efd865cdfc9f30cfdb22e
Pablo Adames <pablo.adames@gmail.com>
2020-05-13 10:33:00 -0600

    more parallelization

--- src/tests.py
+++ src/tests.py
@@ -47,13 +47,13 @@ common_movies = set(data_user_1.movie_id).intersection(data_user_2.movie_id)
 print( "\nNumber of common movies",len(common_movies),'\n')
 
 # creat the subdataframe with only with the common movies
-mask = (data_user_1.movie_id.isin(common_movies))
-data_user_1 = data_user_1[mask]
-print(data_user_1[['title','rating']].head())
+#mask = (data_user_1.movie_id.isin(common_movies))
+#data_user_1 = data_user_1[mask]
+#print(data_user_1[['title','rating']].head())
 
-mask = (data_user_2.movie_id.isin(common_movies))
-data_user_2 = data_user_2[mask]
-print(data_user_2[['title','rating']].head())
+#mask = (data_user_2.movie_id.isin(common_movies))
+#data_user_2 = data_user_2[mask]
+#print(data_user_2[['title','rating']].head())
 
 from scipy.stats import pearsonr
 from scipy.spatial.distance import euclidean
@@ -82,7 +82,8 @@ def SimPearson(dataFrame, user1, user2, min_common_items=1):
     # GET MOVIES OF USER2
     movies_user2=dataFrame[dataFrame['user_id'] == user2 ]
     
-    # FIND SHARED FILMS
+    # the header has ['user_id', 'title', 'movie_id', 'rating', 'release_date', 'sex', 'age', 'for_testing']    
+    # the merge will create rating_x and rating_y as the values from user1 and user2 respectively
     rep=pd.merge(movies_user1, movies_user2, on='movie_id')
     if len(rep)==0:
         return 0    
@@ -97,37 +98,37 @@ def SimPearson(dataFrame, user1, user2, min_common_items=1):
         return res
     
     
-print("""
-|Distance Measure|sim(1,8)|sim(1,31)|
-|Euclidean|{0}|{1}|
-|Pearson|{2}|{3}|"""
-    .format(round(SimEuclid(data, 1, 8), 4), round(SimEuclid(data, 1, 31), 4),\
-            round(SimPearson(data, 1 ,8), 4), round(SimPearson(data, 1, 31), 4)))
+#print("""
+#|Distance Measure|sim(1,8)|sim(1,31)|
+#|Euclidean|{0}|{1}|
+#|Pearson|{2}|{3}|"""
+    #.format(round(SimEuclid(data, 1, 8), 4), round(SimEuclid(data, 1, 31), 4),\
+            #round(SimPearson(data, 1 ,8), 4), round(SimPearson(data, 1, 31), 4)))
 
-movies_user1=data[data['user_id'] ==1 ][['user_id','movie_id','rating']]
-movies_user2=data[data['user_id'] ==8 ][['user_id','movie_id','rating']]
+#movies_user1=data[data['user_id'] ==1 ][['user_id','movie_id','rating']]
+#movies_user2=data[data['user_id'] ==8 ][['user_id','movie_id','rating']]
     
-# FIND SHARED FILMS
-rep=pd.merge(movies_user1 ,movies_user2,on='movie_id')
-x = rep.rating_x + np.random.normal(loc=0.0, scale=0.1, size=len(rep.rating_x))
-y = rep.rating_y + np.random.normal(loc=0.0, scale=0.1, size=len(rep.rating_y))
+## FIND SHARED FILMS
+#rep=pd.merge(movies_user1 ,movies_user2,on='movie_id')
+#x = rep.rating_x + np.random.normal(loc=0.0, scale=0.1, size=len(rep.rating_x))
+#y = rep.rating_y + np.random.normal(loc=0.0, scale=0.1, size=len(rep.rating_y))
     
-a = rep.groupby(['rating_x', 'rating_y']).size()
-x = []
-y = []
-s = []
-for item,b in a.iteritems():
-    x.append(item[0])
-    y.append(item[1])
-    s.append(b*30)
+#a = rep.groupby(['rating_x', 'rating_y']).size()
+#x = []
+#y = []
+#s = []
+#for item,b in a.iteritems():
+    #x.append(item[0])
+    #y.append(item[1])
+    #s.append(b*30)
 
-fig = plt.figure(figsize=(6, 4))
-plt.scatter(x, y, s=s)
-plt.xlabel('Rating User 1')
-plt.ylabel('Rating User ' + str(8))
-plt.axis([0.5,5.5,0.5,5.5])
-plt.savefig("../images/corre18.png", dpi= 300, bbox_inches='tight')
-plt.show()
+#fig = plt.figure(figsize=(6, 4))
+#plt.scatter(x, y, s=s)
+#plt.xlabel('Rating User 1')
+#plt.ylabel('Rating User ' + str(8))
+#plt.axis([0.5,5.5,0.5,5.5])
+#plt.savefig("../images/corre18.png", dpi= 300, bbox_inches='tight')
+#plt.show()
 
 
 movies_user1=data[data['user_id'] ==1 ][['user_id','movie_id','rating']]
@@ -153,7 +154,7 @@ plt.xlabel('Rating User 1')
 plt.ylabel('Rating User ' + str(31))
 plt.axis([0.5,5.5,0.5,5.5])
 plt.savefig("../images/corre131.png", dpi=300, bbox_inches='tight')
-plt.show()
+#plt.show()
 
 
 def assign_to_set(df):
@@ -177,54 +178,138 @@ print ("The test data set has " + str(data_test.shape[0]) + " ratings")
 print ("The data set has " + str(data.movie_id.nunique()) + " movies")
 
 
+def similarity_fun_wrapper(sim_func, user_matrix_all_common_movies, user1, user2):
+    """wraps the function passed in as argument"""
+    if user1 == user2: 
+        return None
+    sim = sim_func(user_matrix_all_common_movies, user1, user2)
+    
+    if(sim < 0):
+        return (user1, user2, 0)
+    else:
+        return (user1, user2, sim)    
+
+
+import multiprocessing as mp
+from multiprocessing import util
+import threading as th
+import time
+
+util.get_logger().setLevel(util.DEBUG)
+
 def compute_rmse(y_pred, y_true):
     """ Compute Root Mean Squared Error. """
     return np.sqrt(np.mean(np.power(y_pred - y_true, 2)))
 
 
+similarity_matrix = {}
+
+def compute_dataframes(sim_func, user1, user_matrix_all_common_movies, all_users):
+    """receives data frame of users who have watched same movies as user1.
+    It does compute the similarity of user1 with each one fot he users that
+    appear in the rows of the dataframe.
+    
+    It returns a tuple: 
+        (user1, user2, similarity_score)"""
+
+    results = []
+    for user2 in all_users:
+        result = similarity_fun_wrapper(sim_func, user_matrix_all_common_movies, user1, user2)
+        results.append(result)
+    return results
+
+
+def collect_result(results):
+    global similarity_matrix
+    #print("Similarity Matrix = {}".format(similarity_matrix))
+    if results is not None:
+        #print(results)
+        for r in results:
+            if r is not None:
+                u1, u2, sim_score = r
+                if u1 not in similarity_matrix.keys():
+                    similarity_matrix.setdefault(u1, {})
+                if u2 not in similarity_matrix[u1].keys():
+                    similarity_matrix[u1].setdefault(u2, sim_score)
+
+                if u2 not in similarity_matrix[u1].keys():
+                    similarity_matrix[u1].setdefault(u2, {})
+                if u1 not in similarity_matrix[u2].keys():
+                    similarity_matrix[u2].setdefault(u1, sim_score)
+                    
+                #print("user1: {0}, user2: {1}, similarity: {2:.4f}".format(u1, u2, similarity_matrix[u1][u2]))
+    
+    
+
 class CollaborativeFiltering:
     """ Collaborative filtering using a custom sim(u,u'). """
     
-    def __init__(self, DataFrame, similarity=SimPearson):
+    def __init__(self, dataFrame, similarity=SimPearson):
         """ Constructor """
         self.sim_method = similarity # Gets recommendations for a person by using a weighted average
-        self.df = DataFrame
-        self.sim = pd.DataFrame(np.sum([0]),columns=data_train.user_id.unique(), index=data_train.user_id.unique())
-
+        self.df = dataFrame
+        #self.sim = pd.DataFrame(np.sum([0]),columns=data_train.user_id.unique(), index=data_train.user_id.unique())
+        self.pool = pool = mp.Pool(mp.cpu_count())
+        
+    def close_pool(self, is_waiting):
+        """terminates the pool in a separate thread"""
+        util.log_to_stderr(level=util.DEBUG)
+        while True:
+            if not is_waiting():
+                self.pool.close()
+                self.pool.terminate()
+                self.pool.join()
+                
+            time.sleep(0.5)
+            
     def learn(self):
         """ Prepare data structures for estimation. Similarity matrix for users """
-        allUsers=set(self.df['user_id'])
-        self.sim = {}
-        for person1 in allUsers:
-            self.sim.setdefault(person1, {})
-            a = data_train[data_train['user_id']==person1][['movie_id']]
-            data_reduced = pd.merge(data_train, a, on='movie_id')
-            for person2 in allUsers:
-                # no es comparem am nosalres mateixos
-                if person1==person2: 
-                    continue
-                self.sim.setdefault(person2, {})
-                if(person1 in self.sim[person2]):
-                    continue # since is a simetric matrix
-                sim = self.sim_method(data_reduced, person1, person2)
-                if(sim<0):
-                    self.sim[person1][person2]=0
-                    self.sim[person2][person1]=0
-                else:
-                    self.sim[person1][person2]=sim
-                    self.sim[person2][person1]=sim
+        all_users = list(set(self.df['user_id']))
+        
+        list_users_common_movies_matrices = []
+        
+        for user1 in all_users[:10]:
+            # get a list of the movies this user has watched
+            movies_watched_by_user1 = self.df[self.df['user_id']==user1][['movie_id']]
+            # get the data of all users who have watched the movies this user has ever watched
+            user_matrix_all_common_movies = pd.merge(data, movies_watched_by_user1, on='movie_id')
+            list_users_common_movies_matrices.append((user1,user_matrix_all_common_movies))
+
+        for user1, matrix_users_common_movies in list_users_common_movies_matrices:
+            self.pool.apply_async(compute_dataframes, 
+                             args=(SimPearson, user1, matrix_users_common_movies, all_users),
+                             callback=collect_result)        
+
+        m = len(list_users_common_movies_matrices)
+        is_waiting = len(similarity_matrix) < m
+        
+        stop_pool = th.Thread(target=self.close_pool, args=(lambda : is_waiting, ))
+        stop_pool.daemon = True
+        stop_pool.start()
+        
+        while is_waiting:
+            print("{0}/{1}".format(len(similarity_matrix), m))
+            time.sleep(5)
+            is_waiting = len(similarity_matrix) < m
+            
+        print("{0}/{1}".format(len(similarity_matrix), m))    
+        
+        self.sim = similarity_matrix
+        
                 
     def estimate(self, user_id, movie_id):
-        totals={}
         movie_users=self.df[self.df['movie_id'] ==movie_id]
         rating_num=0.0
         rating_den=0.0
         allUsers=set(movie_users['user_id'])
         for other in allUsers:
-            if user_id==other: 
-                continue 
+            if user_id == other: 
+                continue
+
+            print("estimating for user1 {} and user2 {}".format(user_id, other))
             rating_num += self.sim[user_id][other] * float(movie_users[movie_users['user_id']==other]['rating'])
             rating_den += self.sim[user_id][other]
+        
         if rating_den==0: 
             if self.df.rating[self.df['movie_id']==movie_id].mean()>0:
                 # return the mean movie rating if there is no similar for the computation
@@ -234,12 +319,16 @@ class CollaborativeFiltering:
                 return self.df.rating[self.df['user_id']==user_id].mean()
         return rating_num/rating_den
     
-import datetime.datetime.now as now
+from datetime import datetime
 
-time1 =now()
+time1 = datetime.now()
 reco = CollaborativeFiltering(data_train)
-time2 = now()
+time2 = datetime.now()
 reco.learn()
-time3= now()
+time3= datetime.now()
 reco.estimate(user_id=2, movie_id=1)    
-time4 = now()
+time4 = datetime.now()
+
+print("Time for building model [hh:mm:ss] = {}".format(time2-time1))
+print("Time for fitting model [hh:mm:ss] = {}".format(time3-time2))
+print("Time for predicting one data point [hh:mm:ss] = {}".format(time4-time3))
